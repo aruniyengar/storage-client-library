4677a2d43512a936226bf327989d3089acfa0474
Arun Iyengar <aruni@us.ibm.com>
2015-09-15 16:24:50 -0400

    Initial commit

---
+++ src/main/java/client/CacheType.java
@@ -0,0 +1,13 @@
+/**
+ * 
+ */
+package client;
+
+/**
+ * @author ArunIyengar
+ * Enum defining cache types
+ *
+ */
+public enum CacheType {
+	SAME_PROCESS, OTHER_PROCESS
+}
---
+++ src/main/java/client/Stats.java
@@ -0,0 +1,13 @@
+/**
+ * 
+ */
+package client;
+
+/**
+ * @author ArunIyengar
+ * Class containing statistics for cache
+ *
+ */
+public interface Stats {
+
+}
